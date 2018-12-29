064985d049eb2c7a720fd6269985d457f62778fe
charlesangus <c.angustaylor@gmail.com>
2018-12-29 01:13:01 -0500

    base class for matte-generating nodes

---
+++ src/DeepCMWrapper.cpp
@@ -0,0 +1,109 @@
+#include "DeepCMWrapper.h"
+
+using namespace DD::Image;
+
+void DeepCMWrapper::findNeededDeepChannels(ChannelSet& neededDeepChannels)
+{
+    DeepCWrapper::findNeededDeepChannels(neededDeepChannels);
+    neededDeepChannels += _auxiliaryChannelSet;
+}
+
+void DeepCMWrapper::_validate(bool for_real)
+{
+    DeepCWrapper::_validate(for_real);
+
+    // add our output channels to the _deepInfo
+    ChannelSet new_channelset;
+    new_channelset = _deepInfo.channels();
+    new_channelset += _processChannelSet;
+    _deepInfo = DeepInfo(_deepInfo.formats(), _deepInfo.box(), new_channelset);
+}
+
+/*
+Do per-sample, channel-agnostic processing. Used for things like generating P
+mattes and so on.
+TODO: probably better to work with a pointer and length, and then this can
+return arrays of data if desired.
+*/
+void DeepCMWrapper::wrappedPerSample(
+    Box::iterator it,
+    size_t sampleNo,
+    float alpha,
+    DeepPixel deepInPixel,
+    float &perSampleData
+    )
+{
+    perSampleData = 1.0f;
+}
+
+
+/*
+The guts. Do any processing on the channel value. The result will be masked
+and mixed appropriately.
+*/
+void DeepCMWrapper::wrappedPerChannel(
+    const float inputVal,
+    float perSampleData,
+    Channel z,
+    float& outData
+    )
+{
+    switch (_operation)
+    {
+        case REPLACE:
+            outData = perSampleData;
+            break;
+        case UNION:
+            outData = inputVal + perSampleData - inputVal * perSampleData;
+            break;
+        case MASK:
+            outData = inputVal * perSampleData;
+            break;
+        case STENCIL:
+            outData = inputVal * (1.0f - perSampleData);
+            break;
+        case OUT:
+            outData = (1.0f - inputVal) * perSampleData;
+            break;
+        case MIN_OP:
+            outData = MIN(inputVal, perSampleData);
+            break;
+        case MAX_OP:
+            outData = MAX(inputVal, perSampleData);
+            break;
+    }
+}
+
+
+void DeepCMWrapper::top_knobs(Knob_Callback f)
+{
+    Input_ChannelSet_knob(f, &_auxiliaryChannelSet, 0, _auxChannelKnobName);
+    Bool_knob(f, &_unpremultPosition, "unpremult_position_data", "unpremult position");
+    Tooltip(f, "Uncheck for ScanlineRender Deep data, check for (probably) "
+    "all other renderers. Nuke stores position data from the ScanlineRender "
+    "node unpremultiplied, contrary to the Deep spec. Other renderers "
+    "presumably store position data (and all other data) premultiplied, "
+    "as required by the Deep spec.");
+    Input_ChannelSet_knob(f, &_processChannelSet, 0, "output");
+    Bool_knob(f, &_premultOutput, "premult_output", "premult output");
+    Tooltip(f, "If, for some reason, you want your mask stored without "
+    "premultipling it, contrary to the Deep spec, uncheck this. "
+    "Should probably always be checked.");
+    Enumeration_knob(f, &_operation, operationNames, "operation");
+    Divider(f, "");
+}
+
+
+void DeepCMWrapper::custom_knobs(Knob_Callback f)
+{
+}
+
+
+const char* DeepCMWrapper::node_help() const
+{
+    return
+    "Parent class for matte-generation nodes.";
+}
+
+static Op* build(Node* node) { return new DeepCMWrapper(node); }
+const Op::Description DeepCMWrapper::d("DeepCMWrapper", 0, build);
---
+++ src/DeepCMWrapper.h
@@ -0,0 +1,71 @@
+#include "DeepCWrapper.h"
+
+#include <string>
+using namespace DD::Image;
+using namespace std;
+
+static const char* auxChannelName = "position_data";
+enum {REPLACE, UNION, MASK, STENCIL, OUT, MIN_OP, MAX_OP};
+static const char* const operationNames[] = {
+    "replace",
+    "union",
+    "mask",
+    "stencil",
+    "out",
+    "min",
+    "max",
+    0
+    };
+
+class DeepCMWrapper : public DeepCWrapper
+{
+    protected:
+        // can override in child classes to change the name of the aux channel knob
+        const char* _auxChannelKnobName;
+
+        // ChannelSet _processChannelSet;
+        ChannelSet _auxiliaryChannelSet;
+        bool _premultOutput;
+        bool _unpremultPosition;
+
+        int _operation;
+
+    public:
+
+        DeepCMWrapper(Node* node) : DeepCWrapper(node)
+            , _auxChannelKnobName("input_data")
+            , _auxiliaryChannelSet(Chan_Black)
+            , _premultOutput(true)
+            , _unpremultPosition(true)
+            , _operation(0)
+        {
+
+        }
+        virtual void findNeededDeepChannels(
+            ChannelSet& neededDeepChannels
+            );
+        virtual void _validate(bool);
+
+        virtual void wrappedPerSample(
+            Box::iterator it,
+            size_t sampleNo,
+            float alpha,
+            DeepPixel deepInPixel,
+            float &perSampleData
+            );
+        virtual void wrappedPerChannel(
+            const float inputVal,
+            float perSampleData,
+            Channel z,
+            float& outData
+            );
+
+        virtual void top_knobs(Knob_Callback f);
+        virtual void custom_knobs(Knob_Callback f);
+        // virtual void knobs(Knob_Callback f);
+
+        static const Iop::Description d;
+        const char* Class() const { return d.name; }
+        virtual Op* op() { return this; }
+        const char* node_help() const;
+};
