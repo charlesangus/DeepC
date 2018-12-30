0b15c9bceb0b1c1574fb80eb9640a99e88615c6f
charlesangus <c.angustaylor@gmail.com>
2018-12-29 21:56:17 -0500

    menu creation

---
+++ python/menu.py
@@ -0,0 +1,16 @@
+import nuke
+
+toolbar = nuke.menu("Nodes")
+DeepCMenu = toolbar.addMenu("DeepC", icon="DeepC.png")
+DeepCDraw = DeepCMenu.addMenu("Draw", icon="ToolbarDraw.png")
+DeepCChannel = DeepCMenu.addMenu("Channel", icon="ToolbarChannel.png")
+DeepCColor = DeepCMenu.addMenu("Color", icon="ToolbarColor.png")
+DeepC3D = DeepCMenu.addMenu("3D", icon="Cube.png")
+DeepCDraw.addCommand("DeepCPMatte", lambda: nuke.createNode("DeepCPMatte", icon="DeepHoldout.png"))
+DeepCDraw.addCommand("DeepCPNoise", lambda: nuke.createNode("DeepCPNoise"), icon="DeepCPNoise.png")
+DeepCDraw.addCommand("DeepCID", lambda: nuke.createNode("DeepCID", icon="DeepCID.png"))
+DeepCChannel.addCommand("DeepCAddChannels", lambda: nuke.createNode("DeepCAddChannels", icon="DeepCAddChannels.png"))
+DeepCChannel.addCommand("DeepCRemoveChannels", lambda: nuke.createNode("DeepCRemoveChannels", icon="DeepCRemoveChannels.png"))
+DeepCColor.addCommand("DeepCGrade", lambda: nuke.createNode("DeepCGrade", icon="DeepCGrade.png"))
+DeepCColor.addCommand("DeepCSaturation", lambda: nuke.createNode("DeepCSaturation", icon="DeepCSaturation.png"))
+DeepC3D.addCommand("DeepCWorld", lambda: nuke.createNode("DeepCWorld", icon="DeepCWorld.png"))
