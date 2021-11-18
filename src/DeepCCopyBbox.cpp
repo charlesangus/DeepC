e6282be335286b879b1c8735d3db77fd9b34600f
falk <falk@kombinat-13b.de>
2021-11-19 00:21:51 +0100

    fiing up copybbox node.

--- src/DeepCCopyBbox.cpp
+++ src/DeepCCopyBbox.cpp
@@ -1,28 +1,31 @@
-// Copyright (c) 2011 The Foundry Visionmongers Ltd.  All Rights Reserved.
-
 #include "DDImage/DeepFilterOp.h"
 #include "DDImage/Knobs.h"
-#include <string>
+
 static const char* CLASS = "DeepCCopyBbox";
 
-using namespace std;
 using namespace DD::Image;
 
 class DeepCCopyBbox : public DeepFilterOp
 {
-
   float _bbox[4];
 
 public:
-
+  
   int minimum_inputs() const { return 2; }
   int maximum_inputs() const { return 2; }
 
   DeepCCopyBbox(Node* node) : DeepFilterOp(node) {
 
-    _bbox[0] = _bbox[2] = 100,
-    _bbox[1] = _bbox[3] = 200;
+    _bbox[0] = 0;
+    _bbox[1] = 0;
 
+    _bbox[2] = input_format().width();
+    _bbox[3] = input_format().height();
+  }
+
+  const char* node_help() const override
+  {
+    return "Copy the bouinding box from one input into the stream.";
   }
 
   const char* input_label(int n, char*) const
@@ -32,49 +35,41 @@ public:
       case 1: return "bbox";
     }
   }
-  const char* node_help() const override{return "Crop deep data in front of or behind certain planes, or inside or outside of a box.";}
-  const char* Class() const override {return CLASS;}
+
+  const char* Class() const override {
+    return CLASS;
+  }
 
   Op* op() override
   {
     return this;
   }
 
-  void knobs(Knob_Callback f) override
-  {
-
-
-  }
 
   void _validate(bool for_real) override
-  {
-    if (input0() && !input1()) {
-      input0()->validate(for_real);
-      _deepInfo = input0()->deepInfo();
-    }else if (input0() && input1()) {
-      input0()->validate(for_real);
-      input1()->validate(for_real);
-
-      DeepOp* in = dynamic_cast<DeepOp*>(Op::input1());
-      _deepInfo = in->deepInfo();
+    {
+    DeepFilterOp::_validate(for_real);
+    DeepOp* second = dynamic_cast<DeepOp*>(Op::input1());
+    if (input0() && second) {
+      second->validate(for_real);
+      DeepInfo d = second->deepInfo();
+      _bbox[0] = d.x();
+      _bbox[1] = d.y();
+      _bbox[2] = d.r();
+      _bbox[3] = d.t();
+      _deepInfo.box().set(floor(_bbox[0] - 1), floor(_bbox[1] - 1), ceil(_bbox[2] + 1), ceil(_bbox[3] + 1));
+    }else if (input0() && !input1()){
+      DeepFilterOp::_validate(for_real);
       _bbox[0] = _deepInfo.x();
-      _bbox[1] = _deepInfo.r();
-      _bbox[2] = _deepInfo.t();
-      _bbox[3] = _deepInfo.y();
-
-      _deepInfo = input0()->deepInfo();
-      // _deepInfo.box().set(floor(_bbox[0]), floor(_bbox[1] ), ceil(_bbox[2]), ceil(_bbox[3] + 1));
-      _deepInfo.box().set(100, 100, 1500, 1500);
-
-    }else {
-          _deepInfo = DeepInfo();
+      _bbox[1] = _deepInfo.y();
+      _bbox[2] = _deepInfo.r();
+      _bbox[3] = _deepInfo.t();
     }
-
   }
 
   bool doDeepEngine(DD::Image::Box box, const ChannelSet& channels, DeepOutputPlane& plane) override
   {
-    if (!input0() && !input1())
+    if (!input0())
       return true;
 
     DeepOp* in = input0();
@@ -89,6 +84,7 @@ public:
     DeepInPlaceOutputPlane outPlane(channels, box);
     outPlane.reserveSamples(inPlane.getTotalSampleCount());
 
+    //samples per pixel after croping
     std::vector<int> validSamples;
 
     for (DD::Image::Box::iterator it = box.begin(), itEnd = box.end(); it != itEnd; ++it) {
@@ -96,9 +92,10 @@ public:
       const int x = it.x;
       const int y = it.y;
 
-      bool inside = (x >= _bbox[0] && x < _bbox[2] && y >= _bbox[1] && y < _bbox[3]);
+      bool inXY = (x >= _bbox[0] && x < _bbox[2] && y >= _bbox[1] && y < _bbox[3]);
 
-      if (inside) {
+      if (!inXY) {
+        // pixel out of crop bounds
         outPlane.setSampleCount(y, x, 0);
         continue;
       }
@@ -109,14 +106,16 @@ public:
       validSamples.clear();
       validSamples.reserve(inPixelSamples);
 
+      // find valid samples
       for (size_t iSample = 0; iSample < inPixelSamples; ++iSample) {
-
         validSamples.push_back(iSample);
       }
 
       outPlane.setSampleCount(it, validSamples.size());
+
       DeepOutputPixel outPixel = outPlane.getPixel(it);
 
+      // copy valid samples to DeepOutputPlane
       size_t outSample = 0;
       for (size_t inSample : validSamples)
       {
