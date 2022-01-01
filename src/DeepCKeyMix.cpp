92f1f3cd4164cd6fc209ca1ed5ea9bc37615c812
falk <falk@kombinat-13b.de>
2022-01-01 19:24:38 +0100

    working validate

--- src/DeepCKeyMix.cpp
+++ src/DeepCKeyMix.cpp
@@ -1,126 +1,228 @@
-/* AdjustBBox node for deep nodes.
+// Copyright (c) 2011 The Foundry Visionmongers Ltd.  All Rights Reserved.
 
-Based on DeepCrop node from the Foundry.
-Falk Hofmann, 11/2021
-
-*/
 #include "DDImage/DeepFilterOp.h"
-#include "DDImage/DeepPixelOp.h"
 #include "DDImage/Knobs.h"
+#include "DDImage/Row.h"
 
-static const char* CLASS = "DeepCKeyMix";
+static const char *CLASS = "DeepCKeyMix";
+static const char *HELP = "DeepCKeyMix";
 
 using namespace DD::Image;
 
-static const char* const bbox_names[] = {"union", "B\tB side", "A\tA side", nullptr};
+static const char *const bbox_names[] = {"union", "B\tB side", "A\tA side", nullptr};
 
-class DeepCKeyMix : public DeepPixelOp
+class DeepCKeyMix : public DeepFilterOp
 {
 
-  ChannelSet channels;
-  Channel maskChannel;
-  bool invertMask;
-  float mix;
-  // what to do with bbox:
-  enum { UNION, BBOX, ABOX };
-  int bbox_type;
+    ChannelSet channels;
+    Channel maskChannel;
+    bool invertMask;
+    float mix;
+    // what to do with bbox:
+    enum
+    {
+        UNION,
+        BBOX,
+        ABOX
+    };
+    int bbox_type;
+    // Iop *_maskOp;
+
+protected:
+    Iop *_maskOp;
+    DeepOp *_aOp;
+    DeepOp *_bOp;
 
 public:
+    DeepCKeyMix(Node *node) : DeepFilterOp(node)
+    {
+        inputs(3);
 
-  DeepCKeyMix(Node* node) : DeepPixelOp(node) {
-
-    inputs(3);
-    channels = Mask_All;
-    maskChannel = Chan_Alpha;
-    invertMask = false;
-    mix = 1;
-    bbox_type = UNION;
-  }
-
-  const char* input_label(int n, char*) const
-  {
-    switch (n) {
-      case 0: return "B";
-      case 1: return "A";
-      case 2: return "mask";
+        channels = Mask_All;
+        maskChannel = Chan_Alpha;
+        invertMask = false;
+        mix = 1;
+        bbox_type = UNION;
     }
-  }
 
-  const char* node_help() const override
-  {
-    return "KeyMix.\n"
-           "Falk Hofmann 11/2021";
-  }
+    const char *node_help() const override { return HELP; }
+    const char *Class() const override { return CLASS; }
+    Op *op() override { return this; }
 
-  const char* Class() const override {
-    return CLASS;
-  }
+    bool test_input(int input, Op *op) const
+    {
+        switch (input)
+        {
+        case 0:
+        case 1:
+            return DeepFilterOp::test_input(input, op);
+        case 2:
+            return dynamic_cast<Iop *>(op) != 0;
+        default:
+            return DeepFilterOp::test_input(input, op);
+        }
+    }
 
-  Op* op() override
-  {
-    return this;
-  }
+    const char *input_label(int n, char *) const
+    {
+        switch (n)
+        {
+        case 0:
+            return "B";
+        case 1:
+            return "A";
+        case 2:
+            return "mask";
+        }
+    }
 
-  void knobs(Knob_Callback f) override
-  {
-    Input_ChannelMask_knob(f, &channels, 1, "channels");
-    Tooltip(f, "Channels to copy from A. Other channels are copied unchanged from B");
-    Input_Channel_knob(f, &maskChannel, 1, 2, "maskChannel", "mask channel");
-    Tooltip(f, "Channel to use from mask input");
-    Bool_knob(f, &invertMask, "invertMask", "invert");
-    Tooltip(f, "Flip meaning of the the mask channel");
-    Float_knob(f, &mix, "mix");
-    Tooltip(f, "Dissolve between B-only at 0 and the full keymix at 1");
-    Enumeration_knob(f, &bbox_type, bbox_names, "bbox", "Set BBox to");
-    Tooltip(f, "Clip one input to match the other if wanted");
-  }
+    void knobs(Knob_Callback f) override
+    {
+        Input_ChannelMask_knob(f, &channels, 1, "channels");
+        Tooltip(f, "Channels to copy from A. Other channels are copied unchanged from B");
+        Input_Channel_knob(f, &maskChannel, 1, 2, "maskChannel", "mask channel");
+        Tooltip(f, "Channel to use from mask input");
+        Bool_knob(f, &invertMask, "invertMask", "invert");
+        Tooltip(f, "Flip meaning of the the mask channel");
+        Float_knob(f, &mix, "mix");
+        Tooltip(f, "Dissolve between B-only at 0 and the full keymix at 1");
+        Enumeration_knob(f, &bbox_type, bbox_names, "bbox", "Set BBox to");
+        Tooltip(f, "Clip one input to match the other if wanted");
+    }
 
-  void _validate(bool for_real) override
-  {
-    DeepFilterOp::_validate(for_real);
-    DeepOp* A = dynamic_cast<DeepOp*>(Op::input1());
-    DeepOp* B = dynamic_cast<DeepOp*>(Op::input0());
+    void _validate(bool for_real) override
+    {
+        _aOp = dynamic_cast<DeepOp *>(Op::input(1));
+        _bOp = input0();
+        _maskOp = dynamic_cast<Iop *>(Op::input(2));
+        if (_bOp != nullptr)
+        {
+            if (_aOp != nullptr && _maskOp != nullptr)
+            {
+                _aOp->validate(for_real);
+                _bOp->validate(for_real);
+                _maskOp->validate(for_real);
 
-    DeepInfo infoA = A->deepInfo();
-    DeepInfo infoB = B->deepInfo();
+                DeepInfo infoA = _aOp->deepInfo();
+                DeepInfo infoB = _bOp->deepInfo();
+                infoB.merge(infoA);
+                _deepInfo = infoB;
+            }
+            else
+            {
+                _deepInfo = _bOp->deepInfo();
+            }
+        }
+        else
+        {
+            DeepFilterOp::_validate(for_real);
+        }
+    }
 
-    infoB.merge(infoA);
-    _deepInfo =infoB;
- 
-  }
+    void getDeepRequests(Box box, const ChannelSet &channels, int count, std::vector<RequestData> &requests)
+    {
+        if (!input0())
+            return;
 
-  // Wrapper function to work around the "non-virtual thunk" issue on linux when symbol hiding is enabled.
-  void getDeepRequests(Box box, const ChannelSet& channels, int count, std::vector<RequestData>& requests) override
-  {
-    DeepPixelOp::getDeepRequests(box, channels, count, requests);
-  }
+        ChannelSet requestChannels = channels;
+        requestChannels += Mask_Deep;
+        requests.push_back(RequestData(input0(), box, requestChannels, count));
 
+        if (_aOp != nullptr)
+        {
+            requests.push_back(RequestData(_aOp, box, requestChannels, count));
+        }
+        if (_maskOp != nullptr)
+        {
+            _maskOp->request(box, maskChannel, count);
+        }
+    }
 
-  bool doDeepEngine(Box box, const ChannelSet& channels, DeepOutputPlane& plane) override
-  {    
-    if (!input0())
-      return true;
+    bool doDeepEngine(DD::Image::Box box, const ChannelSet &channels, DeepOutputPlane &plane) override
+    {
+        // DeepOp *bOp = input0();
+        // if (!bOp)
+        //     return true;
 
-    DeepOp* B = input0();
-    DeepOp* A = dynamic_cast<DeepOp*>(Op::input1());
-    Iop* mask = dynamic_cast<Iop*>(Op::input(2));
+        // DeepOp *aOp = dynamic_cast<DeepOp *>(Op::input1());
+        // // Iop *maskOp = dynamic_cast<Iop *>(Op::input(2));
+        // if (!aOp)
+        //     return false;
+        // // if (_maskOp != NULL)
+        // //     return false;
 
-    DeepPlane inPlane;
+        // DeepPlane aPlane;
+        // DeepPlane bPlane;
 
-    ChannelSet needed = channels;
-    needed += Mask_DeepFront;
+        // // if (!bOp->deepEngine(box, needed, bPlane))
+        // //     return false;
 
-    if (!B->deepEngine(box, needed, inPlane))
-      return false;
+        // // if (!aOp->deepEngine(box, needed, aPlane))
+        // //     return false;
 
+        // DeepInPlaceOutputPlane outPlane(channels, box);
+        // outPlane.reserveSamples(bPlane.getTotalSampleCount());
 
+        // std::vector<int> validSamples;
 
+        // // float maskVal;
+        // // int currentYRow;
+        // // Row maskRow(box.x(), box.r());
 
-    return true;
-  }
+        // // _maskOp->get(box.y(), box.x(), box.r(), maskChannel, maskRow);
+        // // currentYRow = box.y();
 
-  static const Description d;
+        // for (Box::iterator it = box.begin(), itEnd = box.end(); it != itEnd; ++it)
+        // {
+        //     // we have not already gotten this row, get it now
+        //     // _maskOp->get(it.y, box.x(), box.r(), maskChannel, maskRow);
+        //     // maskVal = maskRow[maskChannel][it.x];
+        //     // maskVal = clamp(maskVal);
+        //     // currentYRow = it.y;
+
+        //     DeepPixel bPixel = bPlane.getPixel(it);
+        //     DeepPixel aPixel = aPlane.getPixel(it);
+
+        //     size_t inPixelSamples = bPixel.getSampleCount();
+
+        //     validSamples.clear();
+        //     validSamples.reserve(inPixelSamples);
+
+        //     // find valid samples
+        //     for (size_t iSample = 0; iSample < inPixelSamples; ++iSample)
+        //     {
+        //         validSamples.push_back(iSample);
+        //     }
+
+        //     outPlane.setSampleCount(it, validSamples.size());
+
+        //     DeepOutputPixel outPixel = outPlane.getPixel(it);
+
+        //     // copy valid samples to DeepOutputPlane
+        //     size_t outSample = 0;
+        //     for (size_t inSample : validSamples)
+        //     {
+        //         const float *inData = bPixel.getUnorderedSample(inSample);
+        //         float *outData = outPixel.getWritableUnorderedSample(outSample);
+        //         for (int iChannel = 0, nChannels = channels.size();
+        //              iChannel < nChannels;
+        //              ++iChannel, ++outData, ++inData)
+        //         {
+        //             *outData = *inData;
+        //         }
+        //         ++outSample;
+        //     }
+        // }
+
+        // outPlane.reviseSamples();
+        // mFnAssert(outPlane.isComplete());
+        // plane = outPlane;
+
+        return true;
+    }
+
+    static const Description d;
 };
 
-static Op* build(Node* node) { return new DeepCKeyMix(node); }
-const Op::Description DeepCKeyMix::d(::CLASS, "Image/DeepCKeyMix", build);
+static Op *build(Node *node) { return new DeepCKeyMix(node); }
+const Op::Description DeepCKeyMix::d(::CLASS, 0, build);
